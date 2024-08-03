a182bb1474bdccc920505c109e519a9c70b115a7
Steven Reyes <steven.reyes88@gmail.com>
2024-08-03 15:15:46 +0900

    fixed pages home? placement and moved to correct file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
-  def home?
-    true
+  def home
   end
 end
--- app/policies/pages_policy.rb
+++ app/policies/pages_policy.rb
@@ -5,6 +5,10 @@ class PagesPolicy < ApplicationPolicy
   # code, beware of possible changes to the ancestors:
   # https://gist.github.com/Burgestrand/4b4bc22f31c8a95c425fc0e30d7ef1f5
 
+  def home?
+    true
+  end
+
   class Scope < ApplicationPolicy::Scope
     # NOTE: Be explicit about which records you allow access to!
     # def resolve
