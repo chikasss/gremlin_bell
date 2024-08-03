c1e2434f6c18f08ef8bf4fb40e45950016b472ff
Steven Reyes <steven.reyes88@gmail.com>
2024-08-03 16:32:14 +0900

    first try at routes creation. No landmarks or news feed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -7,6 +7,26 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :users, only: %i[edit show update] do
+    resources :messages, only: %i[index show]
+    resources :follow, only: :create
+      member do
+        get 'saved_trips', to: 'users#saved_trips', as: 'saved_trips'
+      end
+  end
+
+  resources :routes, only: %i[new create index show edit update] do
+    resources :reviews, only: %i[index create show edit update]
+    member do
+      post 'save', to: 'routes#save', as: 'save'
+      get 'map', to: 'routes#map', as: 'map' do
+        collection do
+          get 'export', to: 'routes#export', as: 'export'
+        end
+      end
+      get 'comments', to: 'routes#comments', as: 'comments'
+      post 'comments', to: 'routes#comments', as: 'new_comment'
+    end
+  end
+
 end
