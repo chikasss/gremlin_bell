2b3aa1989ffff8da6e17c6765e22ace8e572b9e0
narumi katayama <narumikataiama@icloud.com>
2024-08-06 16:44:22 +0900

    admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ puts 'Creating admin user...'
 
 User.create!(
   email: 'admin@mail.com',
-  password: 'admin123',  # Assegure-se de usar uma senha segura e real em produção
+  password: 'admin123',
   first_name: 'Admin',
   last_name: 'User',
   about: 'Administrator of the system',
