f54cc85c3292cc026d737bbb934104b00bad31e4
narumi katayama <narumikataiama@icloud.com>
2024-08-06 20:26:55 +0900

    creating reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+before_action :authenticate_user!
+
+  def show
+    @review = Review.find(params[:id])
+  end
+  
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.route = Route.find(params[:route_id])
+    @review.user = current_user
+    if @review.save
+      redirect_to route_path(@review.route)
+    else
+      render :new
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_06_072816) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_06_104414) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -99,12 +99,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_06_072816) do
   create_table "routes", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.string "ride_type"
     t.float "waypoints"
     t.string "videos_url"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ride_type", array: true
     t.index ["user_id"], name: "index_routes_on_user_id"
   end
 
