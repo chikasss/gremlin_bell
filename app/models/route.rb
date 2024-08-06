c6021b879f916b01ca81db21845015be6a6161b7
Chika Sakai <c.sakai1101@gmail.com>
2024-08-06 21:35:27 +0900

    added favoritor things on saved trip

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,5 @@
 class Route < ApplicationRecord
   belongs_to :user
   has_many :reviews
+  acts_as_favoritable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :routes
+  has_many :reviews
+  has_many :comments
+  has_many :bikes
+  acts_as_favoritor
 end
