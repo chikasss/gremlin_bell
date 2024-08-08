b14f9d54ac9d70374ff992c5198f25f9c8212263
Chika Sakai <c.sakai1101@gmail.com>
2024-08-08 21:55:28 +0900

    fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,6 @@ class PagesController < ApplicationController
   after_action :verify_authorized, except: :home
 
   def home
-  #   @bikes = current_user.bikes
+    @bikes = current_user.bikes
   end
 end
