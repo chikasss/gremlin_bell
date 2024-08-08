15e85ce86e9d4fa31043c4154f4d4818f6cceae1
Andrew Beckett <andrewbeckett95@gmail.com>
2024-08-08 23:14:35 +0900

    Styling home page, still ugly

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :set_user, only: [:edit, :update, :show, :saved_trips]
   after_action :verify_authorized, except: :home
 
   def home
     @bikes = current_user.bikes
+    @saved_trips = current_user.all_favorited
   end
+
 end
