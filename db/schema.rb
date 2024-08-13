4e2818077f12ffa1cda413f2bcfe6f1e6e7ff53e
Chika Sakai <c.sakai1101@gmail.com>
2024-08-13 22:17:21 +0900

    jsonb to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_10_231045) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_13_131039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -124,7 +124,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_10_231045) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ride_type", array: true
-    t.jsonb "waypoints", default: []
+    t.string "waypoints", default: [], array: true
     t.index ["user_id"], name: "index_routes_on_user_id"
   end
 
