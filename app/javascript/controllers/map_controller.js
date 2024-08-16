767c2a7949e6f0a502dc363c4a5dd1a2f1797ade
Steven Reyes <steven.reyes88@gmail.com>
2024-08-16 16:44:37 +0900

    added button to map that allows user to find current location

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,6 +13,18 @@ export default class extends Controller {
       zoom: 4.5
     });
 
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+          positionOptions: {
+              enableHighAccuracy: true
+          },
+          // When active the map will receive updates to the device's location as it changes.
+          trackUserLocation: true,
+          // Draw an arrow next to the location dot to indicate which direction the device is heading.
+          showUserHeading: false
+      })
+    );
+
     // let start = null;
     // let end = null;
     let waypoints = [];
