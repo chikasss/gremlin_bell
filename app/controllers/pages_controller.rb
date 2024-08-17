018f047156293cf482c907b7a6d4e94335bbfcc6
narumi katayama <narumikataiama@icloud.com>
2024-08-17 15:31:46 +0900

    50%

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,11 +10,12 @@ class PagesController < ApplicationController
     # @saved_trips = current_user.all_favorited
     
     followed_users_ids = current_user.following.pluck(:id)
-    @routes = Route.where(user_id: followed_users_ids).order(created_at: :desc)
-    @routes_with_photos = @routes.select { |route| route.photos.attached? }.last(3)
-    @users = User.where.not(id: current_user.following.pluck(:id)).order(created_at: :desc).limit(3)
-    @reviews = Review.where(user_id: followed_users_ids).order(created_at: :desc).limit(3)
-    @bikes = Bike.where(user_id: followed_users_ids).order(created_at: :desc).limit(3)
+    @routes = Route.where(user_id: followed_users_ids).order(created_at: :desc).last(10)
+    @routes_with_photos = @routes.select { |route| route.photos.attached? }.last(10)
+    @users = User.where.not(id: current_user.following.pluck(:id)).order(created_at: :desc).limit(5)
+    @reviews = Review.where(user_id: followed_users_ids).order(created_at: :desc).limit(10)
+    @bikes = Bike.where(user_id: followed_users_ids).order(created_at: :desc).limit(10)
+    @coments = Comment.where(user_id: followed_users_ids).order(created_at: :desc).limit(10)
     else
       @routes = Route.none
       @routes_with_photos = []
