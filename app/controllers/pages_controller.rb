09aed06a2a6385ca30e7ab0e07ba19f272864b7e
narumi katayama <narumikataiama@icloud.com>
2024-08-17 10:06:26 +0900

    fixing error in login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
   after_action :verify_authorized, except: :home
 
   def home
+    if user_signed_in?
     # @bikes = current_user.bikes
     @routes = Route.all.order(created_at: :desc)
     # @saved_trips = current_user.all_favorited
@@ -12,5 +13,10 @@ class PagesController < ApplicationController
     @routes = Route.where(user_id: followed_users_ids).order(created_at: :desc)
     @routes_with_photos = @routes.select { |route| route.photos.attached? }.last(5)
     @users = User.all.order(created_at: :desc).last(5)
+    else
+      @routes = Route.none
+      @routes_with_photos = []
+      @users = User.none
+    end
   end
 end
