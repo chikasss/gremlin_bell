25fe3031da5984a8f3e3ec47d302d8946b39b600
Andrew Beckett <andrewbeckett95@gmail.com>
2024-08-22 22:11:38 +0900

    Changed some spelling and grammar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,11 +13,11 @@ class PagesController < ApplicationController
       @reviews = Review.where(user_id: followed_users_ids).order(created_at: :desc).to_a
       @bikes = Bike.where(user_id: followed_users_ids).order(created_at: :desc).to_a
       @comments = Comment.where(user_id: followed_users_ids).order(created_at: :desc).to_a
-  
+
       @routes_with_photos = Photo.includes(:route, :user).where(route: { user_id: followed_users_ids }).map do |photo|
         { route: photo.route, photo: photo, user: photo.user }
-      end      
-      
+      end
+
       # sorted by created_at and shuffled
       @feed_items = (@routes + @reviews + @bikes + @comments + @routes_with_photos)
       .group_by do |item|
