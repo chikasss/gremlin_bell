12b36bec73d9e1a2231996115c23b80e52268057
Steven Reyes <steven.reyes88@gmail.com>
2024-09-16 15:58:03 +0900

    refactored and rephrased some User model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,39 +9,34 @@ user = User.new(
 )
 
 RSpec.describe User, type: :model do
+  it 'user has an email' do
+    expect(user.email).to eq('testuser@mail.com')
+  end
 
-  describe '#initialize' do
-    context 'user validations' do
-      it 'user has an username' do
-        expect(user.username).to eq('TestUsername')
-      end
+  context 'username validations' do
+    it 'user has a username' do
+      expect(user.username).to eq('TestUsername')
+    end
 
-      it 'invalid user without a username' do
-        user.username = nil
-        expect(user.valid?).to eq(false)
-      end
-
-      it 'username is invalid when under 3 characters'  do
-        user.username = "sn"
-        expect(user.errors.messages[:username]).to include("username must be between 3 and 20 characters long")
-      end
-
-      it 'username is invalid when over 20 characters'  do
-        user.username = 'thisisaverylongusername'
-        expect(user.errors[:username]).to include("username must be between 3 and 20 characters long")
-      end
-
-      it 'user has an email' do
-        expect(user.email).to eq('testuser@mail.com')
-      end
-
-      it 'user has a first name' do
-        expect(user.first_name).to eq('Test')
-      end
-
-      it 'user has a last name' do
-        expect(user.last_name).to eq('User')
-      end
+    it 'invalid user without a username' do
+      user.username = nil
+      expect(user.valid?).to eq(false)
+    end
+    it 'username is invalid when under 3 characters'  do
+      user.username = "sn"
+      expect(user.errors.messages[:username]).to include("username must be between 3 and 20 characters long")
+    end
+    it 'username is invalid when over 20 characters'  do
+      user.username = 'thisisaverylongusername'
+      expect(user.errors[:username]).to include("username must be between 3 and 20 characters long")
+    end
+  end
+  context "name validations" do
+    it 'user has a first name' do
+      expect(user.first_name).to eq('Test')
+    end
+    it 'user has a last name' do
+      expect(user.last_name).to eq('User')
     end
   end
 end
