d09c9c4f4ed40c60b65009e91d249703e1def5ea
Steven Reyes <steven.reyes88@gmail.com>
2024-09-16 15:59:25 +0900

    added spacing between tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,19 +22,23 @@ RSpec.describe User, type: :model do
       user.username = nil
       expect(user.valid?).to eq(false)
     end
+
     it 'username is invalid when under 3 characters'  do
       user.username = "sn"
       expect(user.errors.messages[:username]).to include("username must be between 3 and 20 characters long")
     end
+
     it 'username is invalid when over 20 characters'  do
       user.username = 'thisisaverylongusername'
       expect(user.errors[:username]).to include("username must be between 3 and 20 characters long")
     end
   end
+
   context "name validations" do
     it 'user has a first name' do
       expect(user.first_name).to eq('Test')
     end
+
     it 'user has a last name' do
       expect(user.last_name).to eq('User')
     end
