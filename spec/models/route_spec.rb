a228840c95110b57429b69e736a3aa7c0a6e7162
Steven Reyes <steven.reyes88@gmail.com>
2024-09-29 18:13:12 +0900

    created association tests

--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Route, type: :model do
+  let!(:user) do
+    User.create(
+      username: 'TestUsername',
+      email: 'testuser@mail.com',
+      password: 'password',
+      first_name: 'Test',
+      last_name: 'User',
+      prefecture: 'Hokkaido'
+    )
+  end
+
   let!(:route) do
-    Route.new(
+    Route.create(
       title: 'TestRoute',
       videos_url: 'www.youtube.com',
       description: 'This is a test route',
@@ -12,39 +23,64 @@ RSpec.describe Route, type: :model do
         ["141.09836986142776", "42.86675427633369"],
         ["141.1490391486532", "42.95572467766607"]
       ],
-      user_id: 1
+      ride_type: ['Mountainous'],
+      road_condition: 'Paved',
+      recomended_bikes: ['Sport Bike'],
     )
   end
 
-  context 'route with title' do
-    it 'returns a valid route' do
-      expect(route.title).to eq('TestRoute')
+  # it { should belong_to(:user) }
+  # it { should have_many(:comments) }
+  # it { should validate_presence_of(:comments) }
+
+  describe 'associations' do
+    it 'belongs to a user' do
+      r = Route.reflect_on_association(:user)
+      expect(r.macro).to eq(:belongs_to)
+    end
+
+    it 'has many reviews' do
+      r = Route.reflect_on_association(:reviews)
+      expect(r.macro).to eq(:has_many)
+    end
+
+    it 'has many comments' do
+      r = Route.reflect_on_association(:comments)
+      expect(r.macro).to eq(:has_many)
+    end
+
+    it 'has many landmarks' do
+      r = Route.reflect_on_association(:landmarks)
+      expect(r.macro).to eq(:has_many)
+    end
+
+    it 'has many favorites' do
+      r = Route.reflect_on_association(:favorites)
+      expect(r.macro).to eq(:has_many)
+    end
+
+    it 'has many photos' do
+      r = Route.reflect_on_association(:photos)
+      expect(r.macro).to eq(:has_many)
     end
   end
 
-  context 'route without title' do
+  describe 'validations' do
     before { route.title = nil }
-    it 'returns invalid route' do
+    it 'is valid with a title' do
       expect(route.valid?).to eq(false)
     end
-  end
 
-  # context 'route with a user id' do
-  #   it 'returns a valid route' do
-  #     expect(route.valid?).to eq(true)
-  #   end
-  # end
-
-  context 'route without a user id' do
     before { route.user_id = nil }
-    it 'returns invalid route' do
+    it 'is invalid without user' do
       expect(route.valid?).to eq(false)
     end
   end
 
   describe '#minimum_waypoints' do
     before { route.waypoints = ["140.93342678338806", "42.79543274892502"] }
-    it 'a route is invalid when only one waypoint is provided' do
+    it 'returns an invalid route when only one waypoint is provided' do
+      puts route.waypoints.count
       expect(route.valid?).to eq(false)
     end
   end
